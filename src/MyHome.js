d8cd62ecc041b0789cebffdcdd2c9477ade8cd8e
kanthetilakshmisujatha <164356675+kanthetilakshmisujatha@users.noreply.github.com>
2025-05-06 00:29:07 +0530

    Update MyHome.js

--- src/MyHome.js
+++ src/MyHome.js
@@ -1,27 +1,4 @@
-// import React from 'react';
-// // import Header from './components/Header';
-// // import Footer from './components/Footer';
-// import Home from './components/Home';
-// import AboutUs from './components/AboutUs';
-// import Contact from './components/Contact';
-// import Materials from './components/Materials';
-// import Blogs from './components/Blogs';
 
-// const MyHome = () => {
-//   return (
-//     <div>
-
-//       <Home />
-//       <AboutUs />
-//       <Materials />
-//       <Blogs/>
-//       <Contact />
-
-//     </div>
-//   );
-// };
-
-// export default MyHome;import React from 'react';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
